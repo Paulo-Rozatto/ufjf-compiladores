e7a978cbcc0e45d902aa42f607548e2b035970d3
Paulo Rozatto <rozatto.paulo.victor@gmail.com>
2024-09-20 17:22:39 -0300

    Implementada checagem de funcao completa (eu acho)

--- src/main/java/br/ufjf/estudante/singletons/SFunction.java
+++ src/main/java/br/ufjf/estudante/singletons/SFunction.java
@@ -2,15 +2,24 @@ package br.ufjf.estudante.singletons;
 
 public class SFunction extends SType {
   private final SType[] argTypes;
-  private final SType[] returnTypes;
+  private final SType[] returnTypes; // todo: remover se nao usar depois
 
   public SFunction(SType[] argTypes, SType[] returnTypes) {
     this.argTypes = argTypes;
     this.returnTypes = returnTypes;
   }
 
+  public SFunction(SType[] argTypes) {
+    this.argTypes = argTypes;
+    this.returnTypes = null;
+  }
+
   public SType[] getArgTypes() {
     return argTypes;
+  }
+
+  public int getArgsLen() {
+    return argTypes.length;
   }
 
   @Override
