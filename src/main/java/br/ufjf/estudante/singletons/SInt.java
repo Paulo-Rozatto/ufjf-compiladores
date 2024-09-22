b6bd73a1ff86ae491ff0d8ab151ac6353ca92843
Paulo Rozatto <rozatto.paulo.victor@gmail.com>
2024-09-22 12:53:43 -0300

    Checando indices literais

--- src/main/java/br/ufjf/estudante/singletons/SInt.java
+++ src/main/java/br/ufjf/estudante/singletons/SInt.java
@@ -3,7 +3,17 @@ package br.ufjf.estudante.singletons;
 public class SInt extends SType {
   private static final SInt sInt = new SInt();
 
+  private Integer value = null;
+
+  public SInt(int value) {
+    this.value = value;
+  }
+
   private SInt() {}
+
+  public Integer getValue() {
+    return value;
+  }
 
   public static SInt newSInt() {
     return sInt;
